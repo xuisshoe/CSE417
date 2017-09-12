cda6295ca3e1e6b3d39f4cccf14b1fa9f886bfff
mingquan <mingquan@ACCOUNTS.AD.WUSTL.EDU>
2017-09-12 02:02:13 +0000

    adding hw1/

---
+++ hw1/perceptron_experiment.m
@@ -0,0 +1,13 @@
+function [ num_iters bounds] = perceptron_experiment ( N, d, num_samples )
+%perceptron_experiment Code for running the perceptron experiment in HW1
+%   Inputs: N is the number of training examples
+%           d is the dimensionality of each example (before adding the 1)
+%           num_samples is the number of times to repeat the experiment
+%   Outputs: num_iters is the # of iterations PLA takes for each sample
+%            bounds is the theoretical bound on the # of iterations
+%              for each sample
+%      (both the outputs should be num_samples long)
+
+ 
+end
+
---
+++ hw1/perceptron_learn.m
@@ -0,0 +1,11 @@
+function [ w iterations ] = perceptron_learn( data_in )
+%perceptron_learn Run PLA on the input data
+%   Inputs: data_in: Assumed to be a matrix with each row representing an
+%                    (x,y) pair, with the x vector augmented with an
+%                    initial 1, and the label (y) in the last column
+%   Outputs: w: A weight vector (should linearly separate the data if it is
+%               linearly separable)
+%            iterations: The number of iterations the algorithm ran for
+
+end
+
