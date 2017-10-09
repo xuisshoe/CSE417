3b638ac913115d39ad16c77f4c0e83258add5074
mingquan@ACCOUNTS.AD.WUSTL.EDU <mingquan@ACCOUNTS.AD.WUSTL.EDU@fa64eb7a-3e15-496f-a70f-eb50fc3332bc>
2017-10-09 21:41:24 +0000

    adding hw3

---
+++ cse417t_fl17/hw3/logistic_reg.m
@@ -0,0 +1,17 @@
+function [ w, e_in ] = logistic_reg( X, y, w_init, max_its, eta )
+%LOGISTIC_REG Learn logistic regression model using gradient descent
+%   Inputs:
+%       X : data matrix (without an initial column of 1s)
+%       y : data labels (plus or minus 1)
+%       w_init: initial value of the w vector (d+1 dimensional)
+%       max_its: maximum number of iterations to run for
+%       eta: learning rate
+    
+%   Outputs:
+%       w : weight vector
+%       e_in : in-sample error (as defined in LFD)
+
+
+    
+end
+
