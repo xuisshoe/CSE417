3fb8b19a444d76f40709cc4264465747956f781b
hanx940@ACCOUNTS.AD.WUSTL.EDU <hanx940@ACCOUNTS.AD.WUSTL.EDU@fa64eb7a-3e15-496f-a70f-eb50fc3332bc>
2017-11-09 03:59:34 +0000

    my code for hw4

--- cse417t_fl17/hw4/computeLegPoly.m
+++ cse417t_fl17/hw4/computeLegPoly.m
@@ -6,3 +6,17 @@ function [ z ] = computeLegPoly( x, Q )
 %   Output:
 %       z: matrix where each column is the Legendre polynomials of order 0 
 %          to Q, evaluated atthe corresponding x value in the input
+
+for n=1:size(x(:,1))
+    z(n,1) = 1;
+    z(n,2) = x(n);
+    k = 3;
+    while k <= Q+1
+        z(n,k) = (2*k-1)/k * x(n) * z(n,k-1) - (k-1)/k * z(n,k-2);
+        k = k+1;
+    end
+end
+
+
+
+end
