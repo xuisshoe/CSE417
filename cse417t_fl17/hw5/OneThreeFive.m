70623bbe4b689f689414f23630622b37d56c4621
hanx940@ACCOUNTS.AD.WUSTL.EDU <hanx940@ACCOUNTS.AD.WUSTL.EDU@fa64eb7a-3e15-496f-a70f-eb50fc3332bc>
2017-11-21 03:52:02 +0000

    code for hw5

--- cse417t_fl17/hw5/OneThreeFive.m
+++ cse417t_fl17/hw5/OneThreeFive.m
@@ -4,16 +4,6 @@
 load zip.train;
 
 fprintf('Working on the one-vs-three problem...\n\n');
-subsample = zip(find(zip(:,1)==1 | zip(:,1) == 3),:);
-Y = subsample(:,1);
-X = subsample(:,2:257);
-ct = fitctree(X,Y,'CrossVal','on');
-fprintf('The cross-validation error of decision trees is %.4f\n', ct.kfoldLoss);
-bee = BaggedTrees(X, Y, 200);
-fprintf('The OOB error of 200 bagged decision trees is %.4f\n', bee);
-
-
-fprintf('\nNow working on the three-vs-five problem...\n\n');
 subsample = zip(find(zip(:,1)==3 | zip(:,1) == 5),:);
 Y = subsample(:,1);
 X = subsample(:,2:257);
@@ -21,3 +11,23 @@ ct = fitctree(X,Y,'CrossVal','on');
 fprintf('The cross-validation error of decision trees is %.4f\n', ct.kfoldLoss);
 bee = BaggedTrees(X, Y, 200);
 fprintf('The OOB error of 200 bagged decision trees is %.4f\n', bee);
+oob = zeros(1, 200);
+for i=1:150
+    i
+    oob(i) = BaggedTrees(X, Y, i);
+end
+figure(1)
+plot((1:1:150), a, 'r','LineWidth', 2.5);
+xlabel('Number of bagged decision trees');
+ylabel('OOB error');
+title('OOB error of 1 to 150 bagged decision trees on one-vs-three problem');
+
+
+% fprintf('\nNow working on the three-vs-five problem...\n\n');
+% subsample = zip(find(zip(:,1)==3 | zip(:,1) == 5),:);
+% Y = subsample(:,1);
+% X = subsample(:,2:257);
+% ct = fitctree(X,Y,'CrossVal','on');
+% fprintf('The cross-validation error of decision trees is %.4f\n', ct.kfoldLoss);
+% bee = BaggedTrees(X, Y, 200);
+% fprintf('The OOB error of 200 bagged decision trees is %.4f\n', bee);
